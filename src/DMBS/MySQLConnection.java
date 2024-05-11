77a6f4cd6c70a946360120caa2ba6311647a39c8
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-05-11 15:47:39 +0800

    refactorings

--- src/DMBS/MySQLConnection.java
+++ src/DMBS/MySQLConnection.java
@@ -1,3 +1,5 @@
+package DMBS;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,26 @@ import Attacks.Heal;
 import Attacks.Leech;
 import Attacks.SingleAttack;
 import Main.*;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public void Test1(){
         Attack Bash = new Attack("Bash",22,2,1.1,new SingleAttack());
         Attack Bite = new Attack("Bite",41,3,1.16,new Heal());
         Attack Bite2 = new Attack("Bite Lvl 2",10,2,1.33,new SingleAttack());
         Attack Bash2 = new Attack("Bash Lvl 2",40,4,1.2,new SingleAttack());
         Attack Eat = new Attack("Eat",2,1,1,new SingleAttack());
-        Entity Player = new Entity("Co2",1000,21,33);
+        Entity Player = new Entity("Co2",100,21,33);
         Player.addAttack(Bash2)
                 .addAttack(Bite2);
-        Entity Enemy = new Entity("Globlin",450,22,10);
+        Entity Enemy = new Entity("Globlin",100,22,10);
         Enemy.addAttack(Bash)
-            .addAttack(Bite);
+                .addAttack(Bite);
         Helpers.Fight(Player,Enemy);
         Helpers.Fight(Player,Enemy);
     }
