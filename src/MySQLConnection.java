fd35b56b91ca95c5331cfad49ebe58a80d7bb13c
Giles Anthony Villamor II <villamorbudz@gmail.com>
2024-05-11 15:46:07 +0800

    added MySQLConnection

---
+++ src/MySQLConnection.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+
+    public static final String URL =
+            "jdbc:mysql://192.168.1.13:3306/dbvillamorf1";
+    public static final String USERNAME = "client";
+    public static final String PASSSWORD = "";
+    static Connection getConnection(){
+
+
+        Connection c = null;
+        try{
+            c= DriverManager.getConnection(URL, USERNAME,PASSSWORD);
+            System.out.println("DB Connection Success");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+
+
+        Connection c = getConnection();
+        try{
+            c.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
