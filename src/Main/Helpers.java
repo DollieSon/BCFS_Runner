376a9268c16a15b2e244bb4bba60c55e29a53c28
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-05-20 12:52:43 +0800

    Added Anti-Stall Mechanic

--- src/Main/Helpers.java
+++ src/Main/Helpers.java
@@ -1,7 +1,8 @@
 package Main;
 
 public class Helpers {
-    public static void Fight(Cock player, Cock enemy){
+    public static int Fight(Cock player, Cock enemy){
+        int rounds = 0;
         //reset Player Health
         player.resetCurrent();
         //reset Enemy Health
@@ -9,11 +10,11 @@ public class Helpers {
         while(true) {
             if(player.getStat(Cock.StatName.HEALTH, Cock.StatType.CURRENT) <= 0){
                 System.out.println("Player Died");
-                return;
+                return -1;
             }
             if(enemy.getStat(Cock.StatName.HEALTH, Cock.StatType.CURRENT) <= 0){
                 System.out.println("Enemy Died");
-                return;
+                return 1;
             }
             Attack PlayerAttack = player.getAttack();
             Attack EnemyAttack = enemy.getAttack();
@@ -22,12 +23,15 @@ public class Helpers {
             if(player_attackSpeed <= entity_attackSpeed){
                 System.out.println(player.getName() + "Attacks with " + PlayerAttack.getName());
                 PlayerAttack.apply(player,enemy);
+                if(rounds > 75) player.addStat(Cock.StatName.HEALTH, Cock.StatType.BONUS,-(rounds * rounds));
             }else{
                 System.out.println(enemy.getName() + "Attacks with " + EnemyAttack.getName());
                 EnemyAttack.apply(enemy,player);
+                if(rounds > 75) enemy.addStat(Cock.StatName.HEALTH, Cock.StatType.BONUS,-(rounds * rounds));
             }
             System.out.println(player.toString());
             System.out.println(enemy.toString());
+            rounds++;
         }
     }
 }
